a1730ae51d99cf19b55607aac0e0b56952875edf
eyemeroll <ahmad.amirulhafiz@gmail.com>
2020-12-31 12:48:36 +0800

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6161ebc5a69f40c6b1867509df2e2f79'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '472d5974-8b1e-4185-ad20-5ddc3c9731d6'
+};
